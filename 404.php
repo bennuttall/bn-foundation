e0dcdf63a00b1da24c826d799e5a96cb81e2f8e1
Ben Nuttall <ben@bennuttall.com>
2020-05-29 21:01:58 +0100

    Add 404 page

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<div class="row">
+  <h1>404 &mdash; file not found</h1>
+  <div class="small-12 medium-8 columns">
+    <p>No file or page found at this location.</p>
+  </div>
+</div>
+
+<?php get_footer();
