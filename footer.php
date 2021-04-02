ca8aef68254002f4910ffc6b24a9cac73f447c9b
Ben Nuttall <ben@bennuttall.com>
2021-04-02 22:35:32 +0100

    Add RSS feed link, close #1

--- footer.php
+++ footer.php
@@ -4,7 +4,8 @@
       <div class="row">
         Copyright &copy; 2006-<?php echo date('Y'); ?> Ben Nuttall.
         All text on this site is <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC BY-SA</a>.
-        WordPress theme on <a href="https://github.com/bennuttall/bn-foundation">GitHub.</a>
+        WordPress theme is on <a href="https://github.com/bennuttall/bn-foundation">GitHub</a>.
+        You can subscribe to the <a href="/feed/">RSS feed</a>.
       </div>
     </footer>
 
