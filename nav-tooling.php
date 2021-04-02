d0ca25eda97c40f29cb6ec37e764af5215180dd2
Ben Nuttall <ben@bennuttall.com>
2021-04-03 00:29:33 +0100

    Add home button to nav bar

--- nav-tooling.php
+++ nav-tooling.php
@@ -1,5 +1,6 @@
 <div class="top-bar-right">
   <ul class="menu">
+    <li><a class="button" href="/">Home</a></li>
     <li><a class="button" href="https://bennuttall.com/blog/">Blog</a></li>
   	<li><a class="button" href="/">Tooling Blog</a></li>
   	<li><a class="button" href="https://bennuttall.com/talks/">Talks</a></li>
--- nav.php
+++ nav.php
@@ -1,5 +1,6 @@
 <div class="top-bar-right">
   <ul class="menu">
+    <li><a class="button" href="/">Home</a></li>
     <li><a class="button" href="/blog/">Blog</a></li>
   	<li><a class="button" href="https://tooling.bennuttall.com/">Tooling Blog</a></li>
   	<li><a class="button" href="/talks/">Talks</a></li>
