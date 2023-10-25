522e83f036a111e851e41e897a41778e59e70754
Ben Nuttall <ben@bennuttall.com>
2023-10-25 11:14:22 +0100

    Remove extra links

--- nav.php
+++ nav.php
@@ -4,9 +4,6 @@
     <li><a class="button" href="/blog/">Blog</a></li>
   	<li><a class="button" href="https://tooling.bennuttall.com/">Tooling Blog</a></li>
   	<li><a class="button" href="/talks/">Talks</a></li>
-  	<li><a class="button" href="/projects/">Projects</a></li>
     <li><a class="button" href="/about/">About</a></li>
-  	<li><a class="button" href="https://github.com/bennuttall">GitHub</a></li>
-  	<li><a class="button" href="https://twitter.com/ben_nuttall">Twitter</a></li>
   </ul>
 </div>
