532bbfce8aa8f318b571f21f5465344a661bf496
Ben Nuttall <ben@bennuttall.com>
2024-10-05 15:43:36 +0100

    Add mastodon link to footer

--- footer.php
+++ footer.php
@@ -1,17 +1,17 @@
+</div>
+
+<footer>
+  <div class="row">
+    Copyright &copy; 2006-<?php echo date('Y'); ?> Ben Nuttall.
+    All text on this site is <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC BY-SA</a>.
+    WordPress theme is on <a href="https://github.com/bennuttall/bn-foundation">GitHub</a>.
+    You can subscribe to the <a href="/feed/">RSS feed</a>. Find me on <a rel="me" href="https://mastodon.social/@bennuttall">Mastodon</a>.
   </div>
+</footer>
 
-    <footer>
-      <div class="row">
-        Copyright &copy; 2006-<?php echo date('Y'); ?> Ben Nuttall.
-        All text on this site is <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC BY-SA</a>.
-        WordPress theme is on <a href="https://github.com/bennuttall/bn-foundation">GitHub</a>.
-        You can subscribe to the <a href="/feed/">RSS feed</a>.
-      </div>
-    </footer>
-
-    <script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
-    <script src="<?php bloginfo('template_url'); ?>/foundation.min.js"></script>
-    <script>$(document).foundation();</script>
-	<?php wp_footer(); ?>
-  </body>
+<script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/foundation.min.js"></script>
+<script>$(document).foundation();</script>
+  <?php wp_footer(); ?>
+</body>
 </html>
